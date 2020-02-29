128fa86a73eac73ae3f0ca28e6242abac840b463
Emil Nielsen <emilqz@gmail.com>
2020-02-29 15:14:52 +0100

    Renaming

--- src/builder/PersonPetBuilder.java
+++ src/builder/PersonPetBuilder.java
@@ -2,56 +2,60 @@ package builder;
 
 import executor.Model;
 
-public class PersonBuilder extends Builder {
+public class PersonPetBuilder extends Builder {
 
     /**
      * Creates a metamodel instance
      */
+    @Override
     protected void build() {
-        Model model = entity("Person").
-            attribute("first name", "first name", String.class).
-            attribute("last name", "last name", String.class).
-            entityList("Pet").
-                entity("Pet").
-                    attribute("type", "pet1 type", String.class).
-                    attribute("gender", "pet1 gender", String.class).
-                    attribute("age", "pet1 age", int.class).
-                    entityList("Color").
-                        entity("Color").
-                            attribute("color", "pet1 color1", String.class).
-                        end().
-                        entity("Color").
-                            attribute("color", "pet1 color2", String.class).
-                        end().
-                        entity("Color").
-                            attribute("color", "pet1 color3", String.class).
+        Model model =
+            entity("Person").
+                attribute("first name", "first name", String.class).
+                attribute("last name", "last name", String.class).
+                entityList("Pet").
+                    entity("Pet").
+                        attribute("type", "pet1 type", String.class).
+                        attribute("gender", "pet1 gender", String.class).
+                        attribute("age", "pet1 age", int.class).
+                        entityList("Color").
+                            entity("Color").
+                                attribute("color", "pet1 color1", String.class).
+                            end().
+                            entity("Color").
+                                attribute("color", "pet1 color2", String.class).
+                            end().
+                            entity("Color").
+                                attribute("color", "pet1 color3", String.class).
+                            end().
                         end().
                     end().
-                end().
-                entity("Pet").
-                    attribute("type", "pet2 type", String.class).
-                    attribute("age", "pet2 age", int.class).
-                    attribute("age", "pet2 age", int.class).
-                    entityList("Color").
-                        entity("Color").
-                            attribute("color", "pet2 color1", String.class).
-                        end().
-                        entity("Color").
-                            attribute("color", "pet2 color2", String.class).
-                        end().
-                        entity("Color").
-                            attribute("color", "pet2 color3", String.class).
+                    entity("Pet").
+                        attribute("type", "pet2 type", String.class).
+                        attribute("age", "pet2 age", int.class).
+                        attribute("age", "pet2 age", int.class).
+                        entityList("Color").
+                            entity("Color").
+                                attribute("color", "pet2 color1", String.class).
+                            end().
+                            entity("Color").
+                                attribute("color", "pet2 color2", String.class).
+                            end().
+                            entity("Color").
+                                attribute("color", "pet2 color3", String.class).
+                            end().
                         end().
                     end().
                 end().
             end().
         init();
 
-        model.load("src/test_files/petData.csv");
+        model.load("src/resources/petData1.resources");
+        //model.load("src/resources/petData2.resources");
     }
 
-    public void run() {
-        build();
+    public static void main(String[] args) {
+        new PersonPetBuilder().build();
     }
 
 }
